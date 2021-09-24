3a770393de3a77a411140b65681069c3cc3a36d9
Ove Bastiansen <ovebastiansen@outlook.com>
2021-09-24 22:20:11 +0200

    added a same test that compiles in c#10

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -7,11 +7,19 @@ namespace Tests
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void DoesNotCompile()
         {
             IService service = new Service();
 
             Assert.That(() => service.DoSomething(), Throws.InstanceOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void CompilesIn10()
+        {
+            IService service = new Service();
+
+            Assert.That((TestDelegate)(() => service.DoSomething()), Throws.InstanceOf<NotImplementedException>());
         }
     }
 }
